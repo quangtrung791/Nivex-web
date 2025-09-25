40c4cd7dc4826b8ab110831868f658f60d0dfb90
Hoang Tung <89836403+TrHgTung@users.noreply.github.com>
2025-09-25 10:35:03 +0700

    Update content homepage

--- components/sections/Work1.js
+++ components/sections/Work1.js
@@ -13,7 +13,7 @@ export default function Work1() {
                                 {/* <h3 className="heading">How It Work</h3> */}
                                 <h3 className="heading" style={{'textAlign': 'left', 'textTransform': 'uppercase'}}>Giới thiệu về <span style={{'color': '#BCFE08'}}>Nivex</span></h3>
                                 <p className="fs-14 desc"  style={{'textAlign': 'left', 'padding': '0', 'fontWeight': '400'}}>
-                                    <b>Nivex</b> là nền tảng giao dịch tài sản số được hỗ trợ bởi AI. Tối ưu bảo mật & minh bạch phí, nhằm mang đến cho người dùng trải nghiệm giao dịch thông minh và tự động
+                                    <b>Nivex</b> là nền tảng giao dịch tài sản số được hỗ trợ bởi AI. Tối ưu bảo mật & minh bạch phí, nhằm mang đến cho người dùng trải nghiệm giao dịch thông minh và tự động.
                                 </p>
                             </div>
                             <div className="work__main">
@@ -23,11 +23,11 @@ export default function Work1() {
                                     </div>
                                     <div className="content">
                                         {/* <p className="step">Step 1</p> */}
-                                        <Link href="#" className="title fs-28" style={{'fontSize' : '28px', 'fontWeight': '500', 'marginBottom': '5px', 'padding' : '0 15px'}}>GIAO DỊCH SPOT</Link>
+                                        <Link href="#" className="title fs-28" style={{'fontSize' : '28px', 'fontWeight': '500', 'marginBottom': '5px', 'padding' : '0 15px'}}>GIAO DỊCH <br></br>SPOT</Link>
                                         <p className="text fs-14" style={{'padding': '5px 40px 25px 40px'}}>
-                                            <b>Mua/bán theo giá hiện tại, <br></br>sở hữu tài sản thật.</b> <br></br>
-                                            Lệnh <b>Market/Limit</b>, nhiều cặp; <br></br>hỗ trợ <b>DCA/Grid</b>.<br></br>
-                                            Phù hợp <b>người mới</b>, <br></br>có thể <b>rút về ví</b> cá nhân.
+                                            Mua/bán theo giá hiện tại <br></br>
+                                            Sở hữu tài sản thật.<br></br>
+                                            Market/Limit, hỗ trợ DCA/Grid.
                                           
                                         </p>
                                     </div>
@@ -41,9 +41,9 @@ export default function Work1() {
                                         {/* <p className="step">Step 2</p> */}
                                         <Link href="#" className="title" style={{'fontSize' : '28px', 'fontWeight': '500', 'marginBottom': '5px', 'padding' : '0 15px'}}>GIAO DỊCH <br></br>HỢP ĐỒNG</Link>
                                         <p className="text fs-14" style={{'padding': '5px 40px 25px 40px'}}>
-                                            Giao dịch hợp đồng có đòn bẩy, <br></br>không sở hữu coin. <br></br>
-                                            Quản trị rủi ro: dừng lỗ, ký quỹ, <br></br>cảnh báo thời gian thực.<br></br>
-                                            Hợp với người đã hiểu Spot, <br></br>chấp nhận biến động cao.
+                                            Hợp đồng đòn bẩy, không sở hữu coin. <br></br>
+                                            Quản trị rủi ro, dừng lỗ, ký quỹ.<br></br>
+                                            Cảnh báo ký quỹ thời gian thực.
                                         </p>
                                     </div>
                                     {/* <img className="line" src="/assets/images/icon/connect-line.png" alt="" /> */}
@@ -56,9 +56,9 @@ export default function Work1() {
                                         {/* <p className="step">Step 3</p> */}
                                         <Link href="#" className="title" style={{'fontSize' : '28px', 'fontWeight': '500', 'marginBottom': '5px', 'padding' : '0 15px'}}>CHIẾN LƯỢC LỢI NHUẬN <br></br> DO AI DẪN DẮT</Link>
                                         <p className="text fs-14" style={{'padding': '5px 40px 25px 40px'}}>
-                                            <b>Chọn chiến lược</b>, AI <b>hỗ trợ phân tích & gợi ý.</b> <br></br>
-                                            <b>Tự đặt</b> phân bổ, chốt lời/dừng lỗ; <b>dừng sao chép</b> bất cứ lúc nào. <br></br>
-                                            Phù hợp <b>người bận rộn</b> muốn theo <b>chiến lược kỷ luật</b>.
+                                            Chọn chiến lược theo khẩu vị rủi ro <br></br>
+                                            AI hỗ trợ phân tích và gợi ý. <br></br>
+                                            Tự đặt chốt lời/dừng lỗ, dừng sao chép.
                                         </p>
                                     </div>
                                     {/* <img className="line" src="/assets/images/icon/connect-line.png" alt="" /> */}
