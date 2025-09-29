8a95c69c9fd5f0e8bbd9cd649d5034d502487d5c
Hoang Tung <89836403+TrHgTung@users.noreply.github.com>
2025-09-29 12:33:46 +0700

    Homepage: Responsive iPad Pro

--- components/sections/About1.js
+++ components/sections/About1.js
@@ -22,7 +22,7 @@ export default function About1() {
                 <div className="container ai-copy-trade">
                     {/* Các khóa học chuyên sâu */}
                     <div className="row">
-                        <div className="col-xl-6 col-md-12">
+                        <div className="col-xl-6 col-md-12 adaptive">
                             <div className="about__content element-1-e-l" style={{'padding-left': '20px'}} data-aos="fade-up" data-aos-duration={1000}>
                                 <h3 className="heading"><span style={{'color' : '#BCFE08'}}>CÁC KHÓA HỌC CHUYÊN SÂU</span></h3>
                                 <p className="fs-14 decs text-white">
@@ -35,6 +35,16 @@ export default function About1() {
                         <div className="col-xl-6 col-md-12 limit-width">
                             <div className="about_image">                                
                                  <img className="img0-ai pic-1" src="https://learningchain.vn/wp-content/uploads/nivex/framei.png" ></img>
+                            </div>
+                        </div>
+
+                        <div className="col-xl-6 col-md-12 adaptive-small">
+                            <div className="about__content element-1-e-l" style={{'padding-left': '20px'}} data-aos="fade-up" data-aos-duration={1000}>
+                                <h3 className="heading"><span style={{'color' : '#BCFE08'}}>CÁC KHÓA HỌC CHUYÊN SÂU</span></h3>
+                                <p className="fs-14 decs text-white">
+                                   <b>Nivex</b> tự hào góp mặt tại các hội nghị và sự kiện blockchain hàng đầu thế giới, kết nối với các nhà lãnh đạo ngành và mang về những kiến thức chuyên sâu độc quyền cho cộng đồng của mình.
+                                </p>
+                                <Link href="#" className="btn-action" style={{'color':'black', 'padding': '13px 25px'}}>Xem thêm</Link>
                             </div>
                         </div>
                     </div>
