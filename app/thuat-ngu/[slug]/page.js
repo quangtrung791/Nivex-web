121f37b874c451441bb4bdaf4505e9426ebc67da
Hoang Tung <89836403+TrHgTung@users.noreply.github.com>
2025-10-09 10:34:48 +0700

    Fix ul li ol tag

--- app/thuat-ngu/[slug]/page.js
+++ app/thuat-ngu/[slug]/page.js
@@ -12,18 +12,6 @@ export default function ChiTietThuatNgu({ params }) {
     const [term, setTerm] = useState(null)
     const [loading, setLoading] = useState(true)
     
-    // useEffect(() => {
-    //     fetch(`/api/dictionary/${id}`)
-    //     .then((res) => res.json())
-    //     .then((data) => {
-    //         if (data.success) {
-    //              setTerm(data.data)
-    //         }
-    //         setLoading(false)
-    //     })
-    //     .catch(() => setLoading(false))
-    // }, [id])
-    
     useEffect(() => {
         fetch(`/api/dictionary/${slug}`)
             .then((res) => res.json())
@@ -74,28 +62,8 @@ export default function ChiTietThuatNgu({ params }) {
                                         </div>
                                         <div className="content">
                                             <p className="main-text-p">
-                                                {/* <span className="block">{term.description}</span> */}
-                                                <span className="block" dangerouslySetInnerHTML={{ __html: term.description }}></span>
-                                                {/* <span className="block">Thuật ngữ "altcoin" viết tắt của "alternative coin" và được sử dụng để miêu tả bất kỳ loại tiền điện tử nào ngoài Bitcoin. Bitcoin là đồng tiền điện tử phi tập trung đầu tiên, và từ khi ra mắt vào năm 2009, hàng ngàn loại tiền điện tử khác đã được tạo ra, được gọi chung là altcoins.</span>
-
-<span className="block" >
-    Altcoins tương tự như Bitcoin vì chúng là phi tập trung và sử dụng công nghệ blockchain để ghi và xác nhận giao dịch. Tuy nhiên, chúng thường có cơ chế đồng thuận, thuật toán khai thác và các khác biệt kỹ thuật khác. Những đồng tiền này thường có các tính năng, ứng dụng và mục tiêu riêng của chúng.
-Một số altcoins được tạo ra như một nhánh của mã nguồn Bitcoin, có nghĩa là các nhà phát triển đã lấy mã nguồn mở của Bitcoin và sửa đổi để tạo ra một đồng tiền mới. Các altcoin khác được tạo từ đầu, sử dụng mã nguồn và công nghệ blockchain riêng của chúng. Một số ví dụ phổ biến về altcoins bao gồm Ethereum, Litecoin, Ripple và Bitcoin Cash.
-</span>
-
-<span className="block">
-    Một số altcoins được tạo ra để giải quyết các vấn đề cụ thể mà người tạo tin rằng Bitcoin không thể giải quyết được. Ví dụ, một số altcoins tập trung vào thời gian giao dịch nhanh hơn, phí giao dịch thấp hơn hoặc quyền riêng tư và độ ẩn danh tốt hơn. Một số altcoins khác được tạo ra như một phương tiện để gây quỹ cho một dự án hay một công việc cụ thể.
-</span>
-
-<span className="block">
-    Đầu tư vào altcoins có thể rủi ro hơn so với đầu tư vào Bitcoin, vì nhiều altcoins không được củng cố chặt chẽ, có vốn hóa thị trường nhỏ hơn và không được sử dụng rộng rãi như Bitcoin. Tuy nhiên, đầu tư vào altcoins cũng có thể đem lại lợi nhuận cao hơn, vì giá của những đồng tiền này có thể tăng đáng kể trong một thời gian ngắn. Điều này là do altcoins có tính chất đầu cơ cao và thị trường ít phát triển hơn thị trường Bitcoin.
-</span>
-
-<span className="block" >
-    Tóm lại, thuật ngữ "altcoin" được sử dụng để miêu tả bất kỳ loại tiền điện tử nào ngoài Bitcoin. Altcoins tương tự như Bitcoin trong việc phi tập trung và sử dụng công nghệ blockchain, nhưng thường có các cơ chế đồng thuận, thuật toán khai thác và các khác biệt kỹ thuật khác. Một số altcoins được tạo ra như một nhánh của mã nguồn Bitcoin, trong khi các altcoin khác được tạo từ đầu. Nhiều altcoins được tạo ra để giải quyết các vấn đề cụ thể mà Bitcoin không thể giải quyết. Đầu tư vào altcoins có thể rủi ro hơn so với đầu tư vào Bitcoin, nhưng cũng có thể đem lại lợi nhuận cao hơn.    
-</span>   */}
-                                          </p>
-                                            
+                                                <span className="dictionary-details-display-markdown block" dangerouslySetInnerHTML={{ __html: term.description }}></span>
+                                            </p>
                                         </div>
                                         
                                     </div>
--- app/tin-tuc/[id]/page.js
+++ app/tin-tuc/[id]/page.js
@@ -129,74 +129,9 @@ export default function BlogDetails() {
                                                 <Link href="#" className="time">Feb 03, 2021</Link>
                                             </div>
                                         </div> */}
-                                        <div className="content" dangerouslySetInnerHTML={{ __html: news.content }} >
-                                            {/* <h5>What is the Metaverse?</h5> */}
-                                            {/* <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                                Posuere sed nec, vestibulum odio diam amet. In massa auctor
-                                                integer est. Morbi viverra neque sagittis varius. Ornare in
-                                                nibh dui ornare mattis vel. Volutpat aenean enim sit cras
-                                                egestas diam volutpat integer est. Ornare in odio lorem
-                                                pharetra tempor, neque sit. Lacus fermentum urna tellus
-                                                vulputate aliquam nullam amet, posuere. Magna ligula et ut
-                                                sagittis interdum duis. Ut sed tristique tempor nibh bibendum
-                                                accumsan, fringilla. Eget rhoncus in hendrerit nascetur odio
-                                                consectetur enim tristique. In porttitor.
-                                            </p>
-                                           
-                                            <br />
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus
-                                                quis diam integer ridiculus facilisis. Laoreet diam, ipsum
-                                                congue augue vel aenean mus. At felis, quis massa elementum
-                                                tortor dignissim erat. Pharetra eleifend facilisis sit nisi,
-                                                pharetra urna volutpat. Amet lacus sed lorem euismod at diam
-                                                neque, mi. Eget congue id phasellus pulvinar id at enim
-                                                feugiat cursus. Aliquam egestas vulputate semper at cursus
-                                                quam. Augue egestas montes, amet mauris molestie nibh. Aliquam
-                                                viverra nec, ornare vitae. Eu etiam donec morbi purus quis
-                                                quis nibh. Venenatis cras magna.
-                                            </p> */}
-                                            
+                                        <div className="news-details-display-markdown content" dangerouslySetInnerHTML={{ __html: news.content }} >
+                                            {/* Noi dung */}
                                         </div>
-                                        {/* <div className="details-bottom">
-                                            <div className="tags">
-                                                <h6>Tags:</h6>
-                                                <ul>
-                                                    <li><Link href="/blog-grid-v1">Metaverse</Link></li>
-                                                    <li><Link href="/blog-grid-v1">NFT Marketplace</Link></li>
-                                                    <li><Link href="/blog-grid-v1">Virtual Land</Link></li>
-                                                </ul>
-                                            </div>
-                                            <div className="share">
-                                                <h6>Share:</h6>
-                                                <ul>
-                                                    <li>
-                                                        <Link href="#"><i className="fa-brands fa-facebook-f" /></Link>
-                                                    </li>
-                                                    <li>
-                                                        <Link href="#"><i className="fa-brands fa-instagram" /></Link>
-                                                    </li>
-                                                    <li>
-                                                        <Link href="#"><i className="fa-brands fa-youtube" /></Link>
-                                                    </li>
-                                                    <li>
-                                                        <Link href="#"><i className="fa-brands fa-twitter" /></Link>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div> */}
-                                        {/* <form >
-                                            <h6 className="heading">Leave a comment</h6>
-                                            <div className="form-group">
-                                                <input className="form-control" type="text" placeholder="Name" />
-                                                <input className="form-control" type="text" placeholder="Email" />
-                                            </div>
-                                            <div className="form-group">
-                                                <textarea className="form-control" cols={30} rows={10} placeholder="Message" />
-                                            </div>
-                                            <button className="btn-action" type="submit">Send comment</button>
-                                        </form> */}
                                     </div>
                                 </div>
                                 <div className="col-xl-4 col-md-12 right-side-panel-blog-details">
