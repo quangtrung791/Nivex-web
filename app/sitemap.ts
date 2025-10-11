7bd437f1c887d7b8f99f6eda33f7be20086eabed
Hoang Tung <89836403+TrHgTung@users.noreply.github.com>
2025-10-11 11:39:09 +0700

    Custom Sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,11 +17,15 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     'huong-dan-rut-tien',
     'huong-dan-nap-tien',
     'xac-minh-danh-tinh-kyc',
+    'su-kien',
+    'su-kien-tham-gia',
+    'thuat-ngu',
+    'tin-tuc',
   ].map((p) => ({
     url: `${BASE_URL}/${p}`,
     lastModified: new Date(),
-    changeFrequency: 'weekly' as const,
-    priority: 0.7,
+    changeFrequency: 'daily' as const,
+    priority: 0.9,
   }))
 
   // Trang bài viết kiến thức (đổi prefix đường dẫn cho đúng project của bạn)
@@ -36,8 +40,73 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     url: `${BASE_URL}/chi-tiet-kien-thuc/${r.slug}`,  // đổi nếu bạn dùng đường dẫn khác
     lastModified: r.lastmod ?? new Date(),
     changeFrequency: 'weekly' as const,
-    priority: 0.6,
+    priority: 0.8,
   }))
 
-  return [...staticPages, ...knowledge]
+  // trang tin tức
+  const newsRows = await query(`
+    SELECT slug, COALESCE(updated_at, created_at) AS lastmod
+    FROM public.news
+    WHERE status = 'active' AND slug IS NOT NULL
+  `)
+
+  const news = newsRows.map((r: any) => ({
+    url: `${BASE_URL}/tin-tuc/${r.slug}`,
+    lastModified: r.lastmod ?? new Date(),
+    changeFrequency: 'weekly' as const,
+    priority: 0.8,
+  }))
+
+  // trang sự kiện
+  const eventsRows = await query(`
+    SELECT slug, COALESCE(updated_at, created_at) AS lastmod
+    FROM public.event
+    WHERE slug IS NOT NULL
+  `)
+
+  const events = eventsRows.map((r: any) => ({
+    url: `${BASE_URL}/su-kien/${r.slug}`,
+    lastModified: r.lastmod ?? new Date(),
+    changeFrequency: 'weekly' as const,
+    priority: 0.8,
+  }))
+
+  // Sự kiện tham gia
+  const joinedEventsRows = await query(`
+    SELECT slug, COALESCE(updated_at, created_at) AS lastmod
+    FROM public.joined_events
+    WHERE slug IS NOT NULL
+  `)
+
+  const joinedEvents = joinedEventsRows.map((r: any) => ({
+    url: `${BASE_URL}/su-kien-tham-gia/${r.slug}`,
+    lastModified: r.lastmod ?? new Date(),
+    changeFrequency: 'weekly' as const,
+    priority: 0.8,
+  }))
+
+  //Thuật ngữ
+  const dictionaryRow = await query(`
+    SELECT slug, COALESCE(updated_at, created_at) AS lastmod
+    FROM public.dictionary
+    WHERE slug IS NOT NULL
+  `)
+
+  const dictionary = dictionaryRow.map((r: any) => ({
+    url: `${BASE_URL}/thuat-ngu/${r.slug}`,
+    lastModified: r.lastmod ?? new Date(),
+    changeFrequency: 'weekly' as const,
+    priority: 0.8,
+  }))
+
+  return [
+    ...staticPages,
+    ...knowledge,
+    ...news,
+    ...events,
+    ...joinedEvents,
+    ...dictionary,
+  ]
+
+  // return [...staticPages, ...knowledge]
 }
