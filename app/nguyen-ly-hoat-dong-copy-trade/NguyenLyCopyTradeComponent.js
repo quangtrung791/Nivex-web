fa194ddaf67738152e08489361992e64c553d163
Hoang Tung <89836403+TrHgTung@users.noreply.github.com>
2025-10-14 16:20:18 +0700

    Responsive PC screen for Nguyen ly hoat dong Copy Trade

--- app/nguyen-ly-hoat-dong-copy-trade/NguyenLyCopyTradeComponent.js
+++ app/nguyen-ly-hoat-dong-copy-trade/NguyenLyCopyTradeComponent.js
@@ -88,8 +88,80 @@ export default function NguyenLyHoatDongCopyTrade() {
                                 </div>
                             </div>
                         </div>
+                        <div className="section-2-content">
+                            <div className="nguyen-ly-cot-loi">
+                                <div className="title-section-2-container">
+                                    <img src="/assets/images/icon/play.svg" ></img>
+                                    <p className="title-section-2">nguyên lý cốt lõi</p>
+                                </div>
+                                <ul className="ul-text-content">
+                                    <li className="li-text-content" style={{'list-style-type': 'disc', 'color': 'white', 'fontWeight': 300 }}>Mở đồng thời các vị thế Mua (Long) / Bán (Short) để thu lợi nhuận từ các dao động của thị trường.</li>
+                                    <li className="li-text-content" style={{'list-style-type': 'disc', 'color': 'white', 'fontWeight': 300 }}>Kết hợp chiến lược xu hướng Martingale, tự động tăng vị thế một cách thông minh khi xu hướng dữ liệu lớn rõ ràng để tối đa hóa lợi nhuận.</li>
+                                    <li className="li-text-content" style={{'list-style-type': 'disc', 'color': 'white', 'fontWeight': 300 }}>Tự động giảm quy mô vị thế khi thị trường đảo chiều để giảm thiểu rủi ro.</li>
+                                </ul>
+                            </div>
+                            <div className="uu-diem-chien-luoc">
+                                <div className="title-section-2-container">
+                                    <img src="/assets/images/icon/play.svg" ></img>
+                                    <p className="title-section-2">Ưu điểm chiến lược</p>
+                                </div>
+                                <ul className="ul-text-content">
+                                    <li className="li-text-content" style={{'list-style-type': 'disc', 'color': 'white', 'fontWeight': 300 }}>Giao dịch hai chiều bao quát cả thị trường tăng và giảm: có thể kiếm lợi nhuận dù thị trường đi lên hay đi xuống.</li>
+                                    <li className="li-text-content" style={{'list-style-type': 'disc', 'color': 'white', 'fontWeight': 300 }}>Cơ chế bổ sung vị thế linh hoạt: tự động vào thêm lệnh khi thị trường điều chỉnh giảm để trung bình giá.</li>
+                                    <li className="li-text-content" style={{'list-style-type': 'disc', 'color': 'white', 'fontWeight': 300 }}>Hỗ trợ đòn bẩy từ 2 đến 50 lần: đáp ứng nhu cầu của các nhà đầu tư có khẩu vị rủi ro khác nhau.</li>
+                                </ul>
+                            </div>
+                        </div>
                     </section>
-                   
+
+                    <section className="tai-sao-nen-chon-chien-luoc">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-12" style={{'paddingLeft': 0,'paddingRight': 0}}>
+                                    <div className="block-text">
+                                        <h3 className="big-text">Tại sao nên chọn <span className="gradient-text">chiến lược AI theo tín hiệu<br/> từ tổ chức </span>của Nivex?</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                       <div className="block-khoi-div-tai-sao-container flex">
+                            <div className="block-khoi-div-tai-sao block-khoi-div-tai-sao-gray">
+                                <div className="sub-div-sb">
+                                    <img src="/assets/images/icon/human.svg" />
+                                    <p className="sub-title-section-3">Sao chép lệnh AI hoàn toàn tự động</p>
+                                    <p className="sub-content-section-3">Không cần người dùng thao tác thủ công, tự động xác định thời điểm mở/đóng lệnh một cách thông minh.</p>
+                                </div>
+                            </div>
+                            <div className="block-khoi-div-tai-sao">
+                                <div className="sub-div-sb">
+                                    <img src="/assets/images/icon/human_gray.svg" />
+                                    <p className="sub-title-section-3">Chiến lược có tỷ lệ thắng cao</p>
+                                    <p className="sub-content-section-3">Chiến lược được cải tiến và tối ưu hóa liên tục, tỷ lệ thắng trong lịch sử ổn định trên 95%+.</p>
+                                </div>
+                            </div>
+                            <div className="block-khoi-div-tai-sao block-khoi-div-tai-sao-gray">
+                                <div className="sub-div-sb">
+                                    <img src="/assets/images/icon/human.svg" />
+                                    <p className="sub-title-section-3">Cơ chế quản trị rủi ro thông minh</p>
+                                    <p className="sub-content-section-3">Tự động nhận diện các biến động thị trường cực đoan, phòng tránh trước nguy cơ cháy tài khoản.</p>
+                                </div>   
+                            </div>
+                            <div className="block-khoi-div-tai-sao">
+                                <div className="sub-div-sb">
+                                    <img src="/assets/images/icon/human_gray.svg" />
+                                    <p className="sub-title-section-3">Phong cách giao dịch linh hoạt</p>
+                                    <p className="sub-content-section-3">Cung cấp các chiến lược tương ứng cho cả tài khoản theo phong cách Ổn định và Tấn công.</p>
+                                </div>
+                            </div>
+                            <div className="block-khoi-div-tai-sao block-khoi-div-tai-sao-gray">
+                                <div className="sub-div-sb">
+                                    <img src="/assets/images/icon/human.svg" />
+                                    <p className="sub-title-section-3">Đảm bảo an toàn từ nền tảng</p>
+                                    <p className="sub-content-section-3">Hợp tác với các sàn giao dịch tuân thủ quy định toàn cầu, vốn có thể nạp/rút bất cứ lúc nào.</p>
+                                </div>
+                            </div>
+                       </div>
+                    </section>                   
                     
                 </div>
 
