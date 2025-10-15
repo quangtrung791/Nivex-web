b38e37b0e5c6ab4ddfadb9f638e453020e0fac09
Hoang Tung <89836403+TrHgTung@users.noreply.github.com>
2025-10-15 16:57:40 +0700

    Set meta data Năng lực cạnh tranh

--- app/nang-luc-canh-tranh/page.js
+++ app/nang-luc-canh-tranh/page.js
@@ -2,8 +2,8 @@ import Layout from '@/components/layout/Layout'
 import NangLucCanhTranhComponent from './NangLucCanhTranhComponent'
 
 export const metadata = {
-  title: 'Nguyên lý hoạt động Copy Trade | Nivex Hub',
-  description: 'Chiến lược AI cấp tổ chức phân tích thị trường như thế nào.',
+  title: 'Copy Trade và AI Nâng cao - Năng lực cạnh tranh | Nivex Hub',
+  description: 'Khám phá sức mạnh của Copy Trade kết hợp AI Nâng cao cùng Nivex Hub - giải pháp tối ưu giúp nhà đầu tư nâng cao năng lực cạnh tranh, tối đa hóa lợi nhuận và tự động hóa chiến lược giao dịch thông minh.',
   robots: {
         index: true,
         follow: true,
@@ -16,8 +16,8 @@ export const metadata = {
         },
     },
     openGraph: {
-        title: "Nguyên lý hoạt động Copy Trade | Nivex Hub",
-        description: "Chiến lược AI cấp tổ chức phân tích thị trường như thế nào.",
+        title: "Copy Trade và AI Nâng cao - Năng lực cạnh tranh | Nivex Hub",
+        description: "Khám phá sức mạnh của Copy Trade kết hợp AI Nâng cao cùng Nivex Hub - giải pháp tối ưu giúp nhà đầu tư nâng cao năng lực cạnh tranh, tối đa hóa lợi nhuận và tự động hóa chiến lược giao dịch thông minh.",
         url: "https://nivex.vn/huong-dan-tao-lien-ket-gioi-thieu",
         siteName: "Nivex",
         images: [
@@ -25,7 +25,7 @@ export const metadata = {
                 url: "/assets/images/logo/Nivex_icon_bg.png",
                 width: 1200,
                 height: 630,
-                alt: "Chiến lược AI cấp tổ chức phân tích thị trường như thế nào."
+                alt: "Khám phá sức mạnh của Copy Trade kết hợp AI Nâng cao cùng Nivex Hub - giải pháp tối ưu giúp nhà đầu tư nâng cao năng lực cạnh tranh, tối đa hóa lợi nhuận và tự động hóa chiến lược giao dịch thông minh."
             }
         ],
         locale: "vi_VN",
