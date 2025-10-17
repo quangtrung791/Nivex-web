9cb34c9df5bf62a14a219c9ce3a8976d85b9457a
Hoang Tung <89836403+TrHgTung@users.noreply.github.com>
2025-10-17 10:38:39 +0700

    update Tiem nang loi nhuanj

--- app/tim-hieu-tiem-nang-loi-nhuan/TimHieuTiemNangLoiNhuanComponent.js
+++ app/tim-hieu-tiem-nang-loi-nhuan/TimHieuTiemNangLoiNhuanComponent.js
@@ -18,9 +18,6 @@ export default function TimHieuTiemNangLoiNhuan() {
                         <div className="li-xi-nivex-super-title">
                             <h2 className="banner-title">Tìm hiểu <span className="gradient-text">tiềm năng lợi nhuận</span></h2>
                         </div>
-                        {/* <div className="li-xi-nivex-super-text">
-                            <p>Hướng dẫn chi tiết giúp bạn tạo tài khoản Nivex mới <span className="break-line">bằng gmail nhanh chóng và dễ dàng.</span></p>
-                        </div> */}
                     </section>
 
                     <section className="content-tao-bao-li-xi-nivex-huong-dan">
@@ -34,44 +31,42 @@ export default function TimHieuTiemNangLoiNhuan() {
                                     </div>
                                 </div>
                             </div>
-                            {/* <div className="row image-bao-li-xi-container">
-                                <img className="image-bao-li-xi" src='/assets/images/icon/li-xi.svg' />
-                            </div> */}
                         </div>
-                        <div className="huong-dan-cac-buoc-container">
-                            <div className="buoc-huong-dan">
-                                <div className="step-components step-components-mobile-mt">
-                                    <p className="gg-step-component-title">1. Mở ứng dụng Nivex và nhấn vào nút <span className="text-bolder">đăng ký</span></p>
-                                    {/* <p className="step-component-text">Mở Ứng dụng Nivex, nhấn vào biểu tượng <span className="text-bolder">LOGO</span> ở góc trên bên trái, sau đó chọn <span className="text-bolder">Khuyến mãi </span>-&gt; <span className="text-bolder">Lì Xì</span></p> */}
-                                    <div className="group-images-flexible">
-                                        <img src="/assets/images/screenshot/gg-1.svg" className="image-li-xi" />
-                                        <img src="/assets/images/screenshot/gg-2.svg" className="image-li-xi" />
-                                        <img src="/assets/images/screenshot/gg-3.svg" className="image-li-xi" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="buoc-huong-dan">
-                                <div className="step-components">
-                                    <p className="gg-step-component-title">2. Nhập địa chỉ Gmail mới của bạn, sau đó xác minh email theo hướng dẫn hệ thống gửi về hộp thư.</p>
-                                </div>
-                            </div>
-                            <div className="buoc-huong-dan">
-                                <div className="step-components">
-                                    <p className="gg-step-component-title">3. Tạo mật khẩu mạnh để bảo mật tài khoản của bạn.</p>
-                                    <div className="group-images-flexible">
-                                        <img src="/assets/images/screenshot/gg-1.svg" className="image-li-xi" />
-                                        <img src="/assets/images/screenshot/gg-2.svg" className="image-li-xi" />
-                                        <img src="/assets/images/screenshot/gg-3.svg" className="image-li-xi" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="buoc-huong-dan">
-                                <div className="step-components">
-                                    <p className="gg-step-component-title">4. Sau khi xác minh và thiết lập xong mật khẩu, bạn đã đăng ký thành công tài khoản Nivex mới.</p>
-                                </div>
-                            </div>
+                        <div>
+                            <table className="table-tiem-nang-loi-nhuan">
+                                {/* <caption>Cap</caption> */}
+                                <thead className="thead-table-tiem-nang">
+                                    <tr className="tr-table-tiem-nang">
+                                        <th className="th-table-tiem-nang">Vốn người dùng (USD)</th>
+                                        <th className="th-table-tiem-nang">Tần suất giao dịch TB/tháng</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody className="tbody-table-tiem-nang">
+                                    <tr className="tr-table-tiem-nang">
+                                        <td className="td-table-tiem-nang">$200 - $1,000</td>
+                                        <td className="td-table-tiem-nang">15 - 30 lần/tháng</td>
+                                    </tr>
+                                    <tr className="tr-table-tiem-nang">
+                                        <td className="td-table-tiem-nang">$200 - $1,000</td>
+                                        <td className="td-table-tiem-nang">~15 lần/tháng</td>
+                                    </tr>
+                                    <tr className="tr-table-tiem-nang">
+                                        <td className="td-table-tiem-nang">$200 - $1,000</td>
+                                        <td className="td-table-tiem-nang">~10 lần/tháng</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
-                    </section> 
+                        <div className="italic-text-tiem-nang-loi-nhuan special-italic-tiem-nang-ln">
+                            <p>Lưu ý:</p>
+                            <ul>
+                                <li>Mỗi chiến lược tổ chức có logic giao dịch khác nhau, vì vậy tần suất có thể dao động.</li>
+                                <li>Các số liệu trên chỉ mang tính trung bình, không cấu thành cam kết lợi nhuận hay khuyến nghị đầu tư.</li>
+                            </ul>
+                        </div>
+                    </section>
+
                     <section className="ho-tro-truc-tuyen">
                         <p className="title-ho-tro-truc-tuyen">Hỗ trợ trực tuyến</p>
                         <p className="text-ho-tro-truc-tuyen">Truy cập trang web chính thức của <Link className="link-gg-auth" href="https://nivex.vn" >Nivex</Link>, nhấp vào nút <Link href="" className="link-gg-auth">[Liên hệ chúng tôi]</Link> hoặc <Link href="" className="link-gg-auth">[Hỗ trợ trực tuyến]</Link> ở cuối trang.</p>
